e3922424188588429f96365884bf99f3f929dad6
gonzalovazquez <gonzalovazquez010@gmail.com>
2017-05-31 19:12:57 -0400

    Feat: Update current file

--- test/jsondb.test.js
+++ test/jsondb.test.js
@@ -4,30 +4,42 @@ import spies from 'chai-spies';
 import assert from 'assert';
 import chaiAsPromised from 'chai-as-promised';
 
-import DiskDB from '../lib/diskdb';
+import JsonDB from '../lib/jsonDB';
 
 chai.use(chaiAsPromised);
 
-const diskdb = new DiskDB();
+const jsonDB = new JsonDB();
 const expect = chai.expect;
 const should = chai.should();
 
 const personalAccessToken = '9187059e4431a66447b3e123acd60637e741fae7';
 
+const userObject = {
+  "email_address": "gonzalo" + Math.random().toString() + "@cibc.com",
+  "name": "gonzalo vazquez",
+  "invited": false,
+  "github_username": "gonzalovazquez"+ Math.random().toString()
+};
+
 describe('jsonDB module', () => {
 
   it('should failed authentication if not credentials are provided', (done) => {
-    expect(diskdb.auth()).to.be.false;
+    expect(jsonDB.auth()).to.be.false;
     done();
   });
 
   it('should return authenticated client', (done) => {
-    expect(diskdb.auth(personalAccessToken)).to.be.true;
+    expect(jsonDB.auth(personalAccessToken)).to.be.true;
     done();
   });
 
   it('should return the reference of the connected path', (done) => {
-    expect(diskdb.connectToRepo('cibc-api', 'marketplace-admin')).eventually.to.be.an('object').notify(done);
+    expect(jsonDB.connectToRepo('cibc-api', 'marketplace-admin')).eventually.to.be.an('object').notify(done);
+  });
+
+  it.only('should create a blob in Github', (done) => {
+    expect(jsonDB.auth(personalAccessToken));
+    expect(jsonDB.updateBlob('cibc-api', 'marketplace-admin', 'users.json', JSON.stringify(userObject))).eventually.to.be.an('object').notify(done);
   });
 
 });
